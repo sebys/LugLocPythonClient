e74eb5ec848fae9230b0a99b6e73402788999565
sebys <sebyshenzenn@gmail.com>
2017-03-10 11:36:49 -0300

    Change current version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='luglocapiclient',
-    version='0.1.0',
+    version='0.1.1',
     description="Simpleapi client for LugLoc",
     long_description=readme + '\n\n' + history,
     author="Sebis Henzenn",
